b439b0d6c8bdf2151899e960b3748ca962a1330f
shashankgn139 <117936829+shashankgn139@users.noreply.github.com>
2023-03-08 19:24:14 +0530

    levelOrder

--- trees/levelOrder.c
+++ trees/levelOrder.c
@@ -1,82 +1,101 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define SIZE 10
-struct BinarySearchTree
-{
+/*
+*Given an array of elements, construct a complete binary tree from this array in level
+*order fashion.That is ,elements from left in the array will be filled in the tree level wise
+*starting from level 0.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX 10
+
+typedef struct bt {
         int info;
-        struct BinarySearchTree *lptr, *rptr;
-};
-typedef struct BinarySearchTree node;
-// queue Implementaion
-int f = 0, r = -1, count = 0;
-node *q[SIZE]; // #define SIZE 10
-void enqueue(node *address)
+        struct bt *lptr, *rptr;
+} node;
+
+node *queue[MAX];
+int f = 0, r = -1, ct = 0;
+
+void enque(node *ele)
 {
-        r = (r + 1) % SIZE;
-        q[r] = address;
-        count++;
+        r = (r + 1) % MAX;
+        ct++;
+        queue[r] = ele;
 }
-node *dequeue()
+
+node *deque()
 {
-        int preF = f;
-        f = (f + 1) % SIZE;
-        count--;
-        return q[preF];
+        int tmp = f;
+        f = (f + 1) % MAX;
+        ct--;
+        return queue[tmp];
 }
 
 node *getnode(int data)
 {
         node *new1 = malloc(sizeof(node));
-        new1->info = data;
         new1->lptr = new1->rptr = NULL;
+        new1->info = data;
         return new1;
 }
-node *lvlotree(node *root, int a[], int n)
+
+node *createLevelorder(node *root, int a[], int n)
 {
         node *temp;
         int i = 0;
         root = getnode(a[i++]);
-        enqueue(root);
-        while (i < n)
-        {
-                temp = dequeue();
+        enque(root);
+        while (i < n) {
+                temp = deque();
                 temp->lptr = getnode(a[i++]);
-                temp->rptr = getnode(a[i++]);
-                enqueue(temp->lptr);
-                enqueue(temp->rptr);
+                if (i < n)                              //this because the program malfunctions 
+                        temp->rptr = getnode(a[i++]);   //for even number of elements
+                enque(temp->lptr);
+                enque(temp->rptr);
         }
         return root;
 }
 
-void levelorderTraversal(node *rootL)
+void levelOrderDisplay(node *root)
 {
+        node *temp;
         f = 0;
         r = -1;
-        count = 0;
-        node *temp;
-        enqueue(rootL);
-        do
-        {
-                temp = dequeue(); // copied
-                printf("%d  ", temp->info);
-                if (temp->lptr) // GN's
-                        enqueue(temp->lptr);
-                if (temp->rptr) // logic
-                        enqueue(temp->rptr);
-        } while (count > 0);
+        ct = 0;
+        enque(root);
+        while (ct != 0) {
+                temp = deque();
+                printf("%d ", temp->info);
+                if (temp->lptr)
+                        enque(temp->lptr);
+                if (temp->rptr)
+                        enque(temp->rptr);
+        }
+}
+
+void inorder(node *root)
+{
+        if (root != NULL) {
+                inorder(root->lptr);
+                printf("%d ", root->info);
+                inorder(root->rptr);
+        }
+        
 }
 
 void main()
 {
+        int n, a[20];
         node *root = NULL;
-        int a[] = {1, 2, 3, 4};
-        int n = sizeof(a) / sizeof(a[0]);
-        root = lvlotree(root, a, n);
-        printf("Expected output:\n");
-        for (int i = 0; i < sizeof(a) / sizeof(a[0]); i++)
-                printf("%d  ", a[i]);
-        printf(" \n");
-        printf("Level order output after tree generation:\n");
-        levelorderTraversal(root);
+        printf("Ennter the number of array elements : ");
+        scanf("%d", &n);
+        printf("\nenter the array elements\n");
+        for(int i = 0; i < n; i++)
+                scanf("%d", &a[i]);
+        printf("\nlevel order tree\n");
+        root = createLevelorder(root, a, n);
         printf("\n");
+        inorder(root);
+        printf("\n");
+        levelOrderDisplay(root);
+        
 }
